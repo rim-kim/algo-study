6fd31264a045eb95235927404b8e4329682a9607
KyoungYoun Lee <kyoungyounlee416@gmail.com>
2021-04-24 00:02:13 +0200

    stack

--- source/Yeonie/stack_basic.py
+++ source/Yeonie/stack_basic.py
@@ -22,3 +22,89 @@ bottom : 아랫부분
 """
 
 # FixedStack 고정 길이 스택
+
+from typing import Any
+
+# 고정 길이 스택 클래스
+class FixedStack:
+
+    # 비어 있는 FixedStack 에 팝 또는 피크 할 때 내보내는 예외 처리
+    class Empty(Exception):
+        pass
+
+    # 가득한 FixedStack 에 푸시할 때 내보내는 예외 처리
+    class Full(Exception):
+        pass
+
+    # 스택 초기화
+    def __init__(self, capacity: int = 256) -> None:
+        self.stk = [None] * capacity     # 스택본체
+        self.capacity = capacity
+        self.ptr = 0
+
+    # 스택에 쌓여있는 데이터 개수를 반환
+    def __len__(self) -> int:
+        return self.ptr
+
+    # 스택이 비어있는지 판단
+    def is_empty(self) -> bool:
+        return self.ptr <= 0
+
+    # 스택이 가득 차 있는지
+    def is_full(self) -> bool:
+        return self.prt >= self.capacity
+
+    def push(self, value: Any) -> None:
+        if self.is_full():
+            raise FixedStack.Full
+
+        self.stk[self.ptr] = value
+        self.ptr += 1
+
+    def pop(self) -> Any:
+        if self.is_empty():
+            raise FixedStack.Empty
+        self.ptr -= 1
+        return self.stk[self.ptr]
+
+    # 스택에서 데이터를 피크 - 꼭대기 데이터를 들여다 봄
+    def peek(self) -> Any:
+        if self.is_empty():
+            raise FixedStack.Empty
+        return self.stk[self.ptr-1]
+
+    # 스택을 비움 - 데이터 삭제
+    def clear(self) -> None:
+        self.ptr = 0
+
+    # 스택에서 value 를 찾아 인덱스를 반환 , 없으면 -1 반환
+    def find(self, value : Any) -> int:
+        for i in range(self.ptr -1, -1, -1):   # 꼭대기 쪽부터 선형검색
+            if self.stk[i] == value:
+                return i
+        return -1
+
+    # 스택에 있는 value의 개수를 반환
+    def count(self, value: Any) -> bool:
+        c = 0
+        for i in range(self.ptr):  # 바닥 쪽부터 선형 검색
+            if self.stk[i] == value:
+                c += 1
+        return c
+
+    # 스택에 value 가 있는지 판단
+    def __contains__(self, value: Any) -> bool:
+        return self.count(value)
+
+    # 덤프 - 스택 안의 모든 데이터를 바다부터 꼭대기 순으로 출력
+    def dump(self) -> None:
+        if self.is_empty():
+            print('스택이 비어 있습니다.')
+        else:
+            print(self.stk[:self.ptr])
+
+
+
+
+
+
